514c3e0089ec37dc42e164d88b24caf7655f3301
flirtwithcode <flirtwithcode@gmail.com>
2022-07-20 08:26:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-feejcu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-feejcu)
---
+++ src/app/rajesh-demo/rajesh-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rajesh-demo',
+  templateUrl: './rajesh-demo.component.html',
+  styleUrls: ['./rajesh-demo.component.css']
+})
+export class RajeshDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
